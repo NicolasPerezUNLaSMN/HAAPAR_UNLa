c4ffefe1e89d95495b5559fbf45895775ac4323c
AgustinaTarifa <agusstarifa@gmail.com>
2025-06-26 20:58:01 -0300

    bootstrap

--- haapar_unla/urls.py
+++ haapar_unla/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from haapar_unla_app.views import inicio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', inicio),
 ]
