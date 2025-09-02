63ab4d07bd6669894d21d75ebe2231038e87dedf
caro <caroaguirre999@gmail.com>
2025-09-02 16:06:08 -0300

    agrego vista de mi perfil

--- haapar_unla/urls.py
+++ haapar_unla/urls.py
@@ -29,12 +29,9 @@ urlpatterns = [
     path('signup/', views.registro, name='registro'),
     path('logout/', views.cerrar_sesion, name='cerrar_sesion'),
     path('signin/', views.iniciar_sesion, name='iniciar_sesion'),
+    path('perfil/', views.perfil, name='perfil'),
     path('crear-reporte/', views.crear_reporte, name='crear_reporte'),
     path('subsistemas/', views.subsistemas, name='subsistemas'),
-    path('perfil/', views.perfil, name='perfil'),
-    path('perfil/', views.perfil, name='perfil'),
-    path('perfil/', views.perfil, name='perfil'),
-    path('perfil/', views.perfil, name='perfil'),
     path('perfil/', views.perfil, name='perfil'),
 ]
 
