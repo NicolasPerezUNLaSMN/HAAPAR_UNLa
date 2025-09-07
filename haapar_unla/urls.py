98e5cab58363cf384115534322259a7bcaabcf73
caro <115050686+caroeaguirre@users.noreply.github.com>
2025-09-07 16:31:40 -0300

    Implementación de la vista de "Mi Perfil" para usuarios (#48)
    Implementación de la vista de perfil de usuario en la aplicación, permitiendo a los usuarios:
    
    - Ver sus datos personales (nombre, apellido, email).
    - Editar nombre y apellido.
    - Cambiar su contraseña.
    - Eliminar (desactivar) su cuenta.
    - Visualización de mensajes de éxito/error tras acciones.

--- haapar_unla/urls.py
+++ haapar_unla/urls.py
@@ -29,8 +29,10 @@ urlpatterns = [
     path('signup/', views.registro, name='registro'),
     path('logout/', views.cerrar_sesion, name='cerrar_sesion'),
     path('signin/', views.iniciar_sesion, name='iniciar_sesion'),
+    path('perfil/', views.perfil, name='perfil'),
     path('crear-reporte/', views.crear_reporte, name='crear_reporte'),
     path('subsistemas/', views.subsistemas, name='subsistemas'),
+    path('perfil/', views.perfil, name='perfil'),
 ]
 
 handler400 = views.error_400_view
